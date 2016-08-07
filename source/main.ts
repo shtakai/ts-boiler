48023095d536def531af55188dc6b06f19823219
shtakai <shtakai@gmail.com>
2016-08-07 21:43:44 +0900

    base

---
+++ source/main.ts
@@ -0,0 +1,9 @@
+/// <reference path="../typings/index.d.ts" />
+
+
+import * as $ from "jquery";
+import Greeter from "./entities/greeter";
+
+let greeter = new Greeter("world!");
+let msg = greeter.greet();
+$("body").html(`<h1>${msg}</h1>`);
